f1d00dbcbed39436dfe88cf1bbff0eca7f02f409
Allan5829 <allan.farca@gmail.com>
2020-07-07 23:26:03 -0700

    Creates the login form and adds functionality to the get and post login pages.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,9 +20,18 @@ class UsersController < ApplicationController
     end 
 
     get "/login" do 
+        erb :'/users/login'
     end 
 
     post "/login" do 
+        user = User.find_by(:email => params[:email])
+ 
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+			redirect '/' #temp redirect location
+		else
+			redirect '/login'
+		end
     end 
 
     get "/logout" do 
