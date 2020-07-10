f3891a51ec7d291a897e8a13e704b1957068d331
Allan5829 <allan.farca@gmail.com>
2020-07-09 20:51:30 -0700

    Creates GET methods for the reviews controller.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,20 @@
 require 'pry'
 class ReviewsController < ApplicationController
+
+    get "/reviews" do
+        erb :'/reviews/index' 
+    end 
+
+    get "/reviews/new" do 
+        erb :'/reviews/new'
+    end 
+
+    get "/reviews/:id" do
+        erb :'/reviews/show'
+    end 
+
+    get "/reviews/:id/edit" do 
+        erb :'/reviews/edit'
+    end 
+
 end 
